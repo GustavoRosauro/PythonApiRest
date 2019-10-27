2e4dc5a24bd4ddd3a16282fcd5c5f8a0ee33fa0d
GustavoRosauro <gustavosrosauro@gmail.com>
2019-10-27 15:51:25 -0300

    'ProjetoAcabado'

--- PessoaController.py
+++ PessoaController.py
@@ -1,25 +1,41 @@
+from builtins import Exception
+
 from flask import Flask, request, Response, jsonify
 import json
-from PessoaDAO import mostrarPessoas,inserirPessoa
+from PessoaDAO import mostrarPessoas, inserirPessoa, buscarPessoa, removerPessoa, editarPessoa
 from pessoa import Pessoa
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     response = Response(json.dumps(mostrarPessoas()), mimetype="application/json")
     return response
 @app.route('/save', methods=['POST'])
 def save():
     try:
-        """pessoa = {
-            "nome": request.json['nome'],
-            "idade": request.json['idade'],
-            "cpf": request.json['cpf']
-        }"""
         pessoa = Pessoa(request.json['nome'], request.json['idade'], request.json['cpf'])
         inserirPessoa(pessoa)
         resp = jsonify("Inseridos com sucesso")
         return resp
-    except print(Exception):
-        pass
+    except Exception:
+        print(Exception)
+@app.route('/<int:id>')
+def usuario(id):
+    response = Response(json.dumps(buscarPessoa(id)), mimetype="application/json")
+    return response
+@app.route('/delete/<int:id>',methods=['DELETE'])
+def excluir(id):
+    removerPessoa(id)
+    return jsonify("Foi removido com sucesso")
+@app.route('/editar/<int:id>',methods=['PUT'])
+def editar(id):
+    try:
+        pessoa = Pessoa(request.json["nome"], request.json["idade"], request.json["cpf"])
+        editarPessoa(id,pessoa)
+        return jsonify('Pessoa editada com sucesso')
+    except Exception:
+        print(Exception)
+
+app.run()
--- PessoaDAO.py
+++ PessoaDAO.py
@@ -1,3 +1,4 @@
+
 import mysql.connector
 from pessoa import Pessoa
 
@@ -16,4 +17,24 @@ def inserirPessoa(pessoa: Pessoa):
     dados = (pessoa.getNome(), pessoa.getIdade(),pessoa.getCPF())
     cursor.execute(sqlInsert, dados)
     db.commit()
-
+def buscarPessoa(id):
+    cursor = db.cursor()
+    sqlSelect = "SELECT NOME,IDADE,CPF FROM PESSOA WHERE ID =" + str(id)
+    cursor.execute(sqlSelect)
+    resultado = cursor.fetchall()
+    return {"pessoa": resultado}
+def removerPessoa(id):
+    cursor = db.cursor()
+    sqlDelete = "DELETE FROM PESSOA WHERE ID = " + str(id)
+    cursor.execute(sqlDelete)
+    db.commit()
+def editarPessoa(id,pessoa:Pessoa):
+    cursor = db.cursor()
+    sqlUpdate = "UPDATE PESSOA " \
+                "SET NOME = %s, " \
+                "IDADE = %s, " \
+                "CPF = %s " \
+                "WHERE ID = "+ str(id)
+    dados = (pessoa.getNome(),pessoa.getIdade(),pessoa.getCPF())
+    cursor.execute(sqlUpdate, dados)
+    db.commit()
